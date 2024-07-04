ac9a400486cbd693253872bdc635cdce60b38f54
action <action@github.com>
2024-07-04 07:23:32 +0000

    Add decoded output file

--- output.py
+++ output.py
@@ -1,58 +1,79 @@
-#2024-07-04 07:22:44
+#2024-07-04 07:23:21
 import requests
-import os
+import json
 import time
+import os
 import random
-import hashlib
-class yuanshen():
+requests.packages.urllib3.disable_warnings()
+code="qdjf"
+ver="1.0.0"
+def version():
+ try:
+  txt=requests.get("https://gitee.com/HuaJiB/yuanshen34/raw/master/pubilc.txt").text
+  print(txt)
+  url=f"http://101.132.127.171/api/huaji/?version={ver}&code={code}"
+  r=json.loads(requests.get(url).text)
+  if r["ok"]:
+   if r["update"]:
+    print(f"有新版本请更新,当前版本:{ver},最新版本:",r["latest_version"])
+    exit()
+   else:
+    print(f"当前版本为最新版本或不用更新,当前版本:{ver},最新版本:",r["latest_version"])
+    print("更新日志:",r["data"]["update_note"])
+  else:
+   print("脚本已关闭")
+ except:
+  print("服务器失联....")
+class yuanshen:
  def __init__(self,cookie):
   self.cookie=cookie
-  self.h={"Host":"app.zhuanbang.net","accept":"application/json, image/webp","user-agent":"Mozilla/5.0 (Linux; Android 12; M2104K10AC Build/SP1A.210812.016; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/96.0.4664.104 Mobile Safari/537.36 HuoNiuFusion/1.25.0_231652","x-requested-with":"XMLHttpRequest","sec-fetch-site":"same-origin","sec-fetch-mode":"cors","sec-fetch-dest":"empty","referer":"https://app.zhuanbang.net/assist/activity/47","accept-language":"zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7","accept-encoding":"gzip","Cookie":f"NiuToken={self.cookie}"}
- def sign_(self):
-  d=f"{self.csrftoken}#{self.sessionId}#{self.time}"
-  byte_string=d.encode('utf-8')
-  sha1=hashlib.sha1()
-  sha1.update(byte_string)
-  sign=sha1.hexdigest()
-  return sign
- def video(self,key):
-  i=0
-  while True:
-   i+=1
-   url=f"https://app.zhuanbang.net/{key}/launch?_random={int(time.time() * 1000)}&type=slide"
-   r=requests.get(url,headers=self.h).json()
-   if r['code']==0:
-    print(f"第[{i}]个红包获取信息成功")
-    self.csrftoken=r['data']['extArgs']['csrfToken']
-    self.sessionId=r['data']['extArgs']['sessionId']
-    self.time=int(time.time())
-    url=f"https://app.zhuanbang.net/{key}/award/grant?_t={self.time}"
-    data={"csrfToken":f"{self.csrftoken}","deviceId":f"{self.sessionId}","timestamp":f"{self.time}","sign":f"{self.sign_()}"}
-    r=requests.post(url,headers=self.h,data=data).json()
-    if r['code']==0:
-     print(f"第[{i}]个红包领取成功,获得[{r['data']['awardMoney']}]元")
-    else:
-     print(f"第[{i}]个红包领取失败---[{r['msg']}]")
-     break
+  self.url='https://qidian.hanhoukeji.com'
+  self.headers={"Host":"qidian.hanhoukeji.com","Connection":"keep-alive","charset":"utf-8","User-Agent":"Mozilla/5.0 (Linux; Android 13; 23054RA19C Build/TP1A.220624.014; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/116.0.0.0 Mobile Safari/537.36 XWEB/1160083 MMWEBSDK/20231202 MMWEBID/98 MicroMessenger/8.0.47.2560(0x28002F30) WeChat/arm64 Weixin NetType/5G Language/zh_CN ABI/arm64 MiniProgramEnv/android","content-type":"application/json","access-token":self.cookie,"version":"1.5.19","platform":"MP-WEIXIN","Referer":"https://servicewechat.com/wx3aa7193831495d74/102/page-frame.html"}
+ def task(self):
+  self.video()
+  self.eat()
+  self.user()
+ def video(self):
+  url=f'{self.url}/index.php?s=/api/seead/detail'
+  re=json.loads(requests.get(url,headers=self.headers).text)
+  times=re["data"]["user_count"]
+  times=5-int(times)
+  if times==0:
+   print("☑️ 当日视频上限")
+  for i in range(times):
+   url=f'{self.url}/index.php?s=/api/seead/getOrderNo'
+   re=json.loads(requests.get(url,headers=self.headers).text)
+   self.order_no=re["data"]["order_no"]
+   time.sleep(random.randint(25,30))
+   url=f'{self.url}/index.php?s=/api/seead/open&order_no={self.order_no}'
+   r=json.loads(requests.get(url,headers=self.headers).text)
+   if r["message"]=="success":
+    print("✅ 观看视频成功，获得积分:",r["data"]["ponits"])
    else:
-    print(f"第[{i}]个获取红包信息失败---[{r['msg']}]")
-    break
-   if i>=21:
-    break
-   time.sleep(random.randint(20,48))
- def main(self):
-  print("===========开始执行快手刷视频===========")
-  self.video("kwai_video")
-  print("===========快手刷视频执行完毕===========")
-  print("===========开始执行抖音刷视频===========")
-  self.video("pangle_video")
-  print("===========抖音刷视频执行完毕===========")
+    print("❌ 观看视频失败",r["message"])
+ def eat(self):
+  current_hour=int(time.strftime("%H"))
+  if(7<=current_hour<9)or(12<=current_hour<14)or(18<=current_hour<21):
+   print("☑️ 当前时间在三餐时间")
+   url=f'{self.url}/index.php?s=/api/blebag/openBag'
+   re=json.loads(requests.get(url,headers=self.headers).text)
+   if re["message"]=="success":
+    print("✅ 领取三餐红包成功，获得积分:",re["data"]["ponits"])
+   else:
+    print("❌ 领取三餐红包失败",re["message"])
+  else:
+   print("☑️ 当前时间不在三餐时间")
+ def user(self):
+  url=f'{self.url}/index.php?s=/api/user/info'
+  re=json.loads(requests.get(url,headers=self.headers).text)
+  print("☑️ 当前总积分:",re["data"]["userInfo"]["points"],"=",re["data"]["userInfo"]["points"]/100,"元")
 if __name__=='__main__':
+ version()
  cookie=''
  if not cookie:
-  cookie=os.getenv("yuanshen_zb")
+  cookie=os.getenv("yuanshen_qdjf")
   if not cookie:
-   print("⛔️请设置环境变量:yuanshen_zb")
+   print("请设置环境变量:yuanshen_qdjf")
    exit()
  cookies=cookie.split("@")
  print(f"一共获取到{len(cookies)}个账号")
@@ -60,6 +81,6 @@ if __name__=='__main__':
  for cookie in cookies:
   print(f"\n--------开始第{i}个账号--------")
   main=yuanshen(cookie)
-  main.main()
-  print(f"--------第{i}个账号执行完毕--------")
-  i+=1
+  main.task()
+ print(f"--------第{i}个账号执行完毕--------")
+ i+=1

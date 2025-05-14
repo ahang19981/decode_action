1af46ae0c2cac166472ae7c3a5ff9cd2a9f4200e
action <action@github.com>
2025-05-14 09:20:42 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,4 +1,4 @@
-//Wed May 14 2025 00:51:01 GMT+0000 (Coordinated Universal Time)
+//Wed May 14 2025 09:20:42 GMT+0000 (Coordinated Universal Time)
 //Base:https://github.com/echo094/decode-js
 //Modify:https://github.com/smallfawn/decode_action
 const ilIIl = new iI1I("联通517"),
